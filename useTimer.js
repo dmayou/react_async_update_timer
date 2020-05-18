b41dba795c5bb96f7d4c7c2086612a5e9e5635eb
David Mayou <davidpmayou@gmail.com>
2020-05-18 13:20:51 -0500

    add parameters maxUpdates, callbackWait

--- useTimer.js
+++ useTimer.js
@@ -3,33 +3,46 @@
 // - remaining time is checked/updated every {increment} ms
 // - {callBack} defined in separate lexical scope and should be async
 // - {useActualElapsed}: flag for how to calculate elapsed time
+// - {maxUpdates}: maximum number of times to call callBack, including initial.
+//                 Cannot be zero. Null = infinite.
+// - {callbackWait}: if true, call callBack initially after timer expires
 // - return: remaining time in ms
 
 import { useRef, useState, useEffect } from 'react';
 
-const useTimer = (timeOut, increment, callBack, useActualElapsed = false) => {
+const useTimer = (timeOut, increment, callBack, useActualElapsed = false, maxUpdates = null, callbackWait = false) => {
   const [actualStartTime, setStartTime] = useState(Date.now());
-  const [remainingTime, setRemainingTime] = useState(0);
+  const [remainingTime, setRemainingTime] = useState(callbackWait ? timeOut : 0);
   const timer = useRef(null);
-  
+  const timesToUpdate = useRef(maxUpdates);
+  const updatesExhausted = useRef(maxUpdates === 0);
+
   useEffect(() => {
     timer.current = setTimeout(() => {
-      const elapsedTime = useActualElapsed ? 
+      const elapsedTime = useActualElapsed ?
         timeOut - (Date.now() - actualStartTime) : // actual elapsed time
         remainingTime - increment; // elapsed time subject to background suspend
-      setRemainingTime(elapsedTime);
+      if (!updatesExhausted.current) {
+        setRemainingTime(elapsedTime);
+      } else {
+        setRemainingTime(0); // when all updates expired, remaining time is 0
+      }
     }, increment);
-    
-    if (remainingTime <= 0) { // works because actual intervals are never shorter than increment
+
+    if (remainingTime <= 0 // works because actual intervals are never shorter than increment
+      && !updatesExhausted.current
+    ) {
       setStartTime(Date.now());
       setRemainingTime(timeOut);
       callBack();
+      timesToUpdate.current && timesToUpdate.current--;
+      updatesExhausted.current = timesToUpdate.current === 0;
     }
-    
+
     return () => clearTimeout(timer.current); // cleanup
-    
+
   }, [remainingTime]);
-  
+
   return remainingTime;
 }
 
